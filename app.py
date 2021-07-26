9cda6f20e85b0aaabc66c60153c5c3026974b687
SeQaLy <seqaly@SeQaLynoMacBook-Pro.local>
2021-07-26 10:44:50 +0900

    fix ip

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def index():
     return render_template( "index.html", shop_data=shop_data, stock_data=stock_data )
 
 if __name__ == "__main__":
-    app.run( host="127.0.0.1", port=int( os.environ.get( "PORT", 5000 ) ), debug=True ) 
+    app.run( host="0.0.0.0", port=int( os.environ.get( "PORT", 5000 ) ), debug=True ) 
